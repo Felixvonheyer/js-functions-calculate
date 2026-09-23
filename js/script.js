fa1806c7935c56602576216739a253e95113444b
Felixvonheyer <felix.vonheyer@gmail.com>
2026-09-23 13:11:02 +0200

    opgave 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
+"use strict";
 // Skriv "use strict" herunder
 
+
+
+
 function beregnSum(number1, number2) {
-    // Funktionen skal beregne summen af de to tal og udskrive resultatet i konsollen, fx: "Summen af 15 og 27 er 42"
+    // Funktionen skal beregne summen af de to tal og 
+    // udskrive resultatet i konsollen, 
+    // fx: "Summen af 15 og 27 er 42"
+  const finalSum = number1 + number2
+  console.log(finalSum);
+}
+
+function beregnDifference (number1,number2) {
+
+const difference = number1 - number2
+console.log(difference);
+
 
 }
+
+
+beregnSum(10,8);
+beregnDifference(20,8)
 
 
 // Skriv en funktion, der hedder beregnDifference, og som tager to parametre: number1 og number2
